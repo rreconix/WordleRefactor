70836992bb41c3eed652ea25a8b9e597796c3f93
felixhansen1 <felix.hansen1007@gmail.com>
2022-12-03 20:04:35 -0500

    update

--- app/js/Game.js
+++ app/js/Game.js
@@ -2,80 +2,204 @@ export default class Game {
 	constructor() {
 		this.darkTheme = true
 		this.hardMode = false
+		this.gameOver = false
+
 		this.attempts = 0
+
+		this.winMessages = [
+			"Genius",
+			"Magnificent",
+			"Impressive",
+			"Splendid",
+			"Great",
+			"Phew",
+		]
+		this.currentGuess = []
+		this.modalQueue = []
 	}
 
-	generateWord() {}
+	init() {
+		this.createGrid()
+		return this
+	}
 
-	validateWord(word) {
-		//5 letters long
-		//valid word
+	async generateWord() {
+		if (this.validAnswers != null) {
+			return this.validAnswers[
+				Math.floor(Math.random() * this.validAnswers.length)
+			]
+		} else {
+			const response = await fetch("../nytwordle_dictionary/valid_answers.txt")
+			const res = (await response.text()).split("\r\n")
+			this.validAnswers = res
+
+			return this.generateWord()
+		}
+	}
+
+	async validateWord(word) {
+		if (word.length == 5) {
+			if (this.dictionary != null) {
+				if (this.dictionary.includes(word)) {
+					return true
+				} else {
+					this.displayModal("Not in word list")
+					return false
+				}
+			} else {
+				const response = await fetch("../nytwordle_dictionary/dictionary.txt")
+				const res = (await response.text()).split("\r\n")
+				this.dictionary = res
+
+				return this.validateWord(word)
+			}
+		} else {
+			this.displayModal("Not enough letters")
+			return false
+		}
 	}
 
 	createGrid() {
 		const grid = document.querySelector(".grid")
-		for(let i = 0; i < 5 * 6; i++){
-			const gridItem = document.createElement("div")
-			gridItem.className = "gridItem"
-			grid.append(gridItem)
-		}
-	}
-
-	createKeys() {
-		const keys = [
-			["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
-			["", "a", "s", "d", "f", "g", "h", "j", "k", "l", ""],
-			[["enter"], "z", "x", "c", "v", "b", "n", "m", ["←"]],
-		]
-		const keyboard = document.querySelector(".keyboard")
-		for (const row of keys) {
-			const rowElement = document.createElement("div")
-			rowElement.className="row"
-			for (const text of row) {
-				if (text == "") {
-					const keyElement = document.createElement("div")
-					keyElement.className = "spacer"
-					rowElement.append(keyElement)
-				} else {
-					const keyElement = document.createElement("button")
-					keyElement.className = "key" + (Array.isArray(text) ? " double" : "")
-					keyElement.textContent = text[0]
-					rowElement.append(keyElement)
-				}
+		for (let i = 0; i < 6; i++) {
+			const row = document.createElement("div")
+			for (let i = 0; i < 5; i++) {
+				const gridItem = document.createElement("div")
+				gridItem.className = "tile"
+				gridItem.dataset.state = "empty"
+				gridItem.style.animationDelay = `${i * 150}ms`
+				row.append(gridItem)
 			}
-			keyboard.append(rowElement)
+			grid.append(row)
 		}
 	}
 
-	checkLetters(guess, answer) {
-		let temp = [...answer]
-		return guess.split("").reduce(
-			(pv, cv, i) => {
-				if (answer[i] == cv) {
-					pv[i] = 2
-				} else if (temp.includes(cv)) {
-					pv[i] = 1
-					temp.splice(answer.indexOf(cv), 1)
-				}
+	async checkLetters(guess, answer) {
+		const amountOfGreens = (cv) =>
+			answer.split("").filter((letter, i) => letter == cv && guess[i] == letter)
+				.length
 
+		const result = guess.split("").reduce(
+			(pv, cv, i, arr) => {
+				if (answer[i] == cv) {
+					pv[i] = "correct"
+				} else if (
+					answer.includes(cv) &&
+					answer.split("").filter((x) => x == cv).length - amountOfGreens(cv) >=
+						arr.slice(0, i + 1).filter((x) => x == cv).length
+				) {
+					pv[i] = "present"
+				}
+				pv[i] = [pv[i], cv]
 				return pv
 			},
-			[...Array(5).fill(0)]
+			[...Array(5).fill("absent")]
 		)
+
+		await this.colorTiles(result)
 	}
 
-	colorTiles(word) {}
+	async colorTiles(colorStates) {
+		const tiles = this.currentRow.children
+		const keys = [...document.querySelectorAll(".key")].filter(
+			(node) => !node.classList.contains("double")
+		)
 
-	displayModal(msg){
+		// GRID ITEMS
 
+		const gridFlip = new Promise((resolve, reject) => {
+			colorStates.forEach(([state], index) => {
+				const tile = tiles[index]
+				tile.classList.add("flip")
+
+				const animationTime = getComputedStyle(tile)
+					.getPropertyValue("--flip-duration")
+					.slice(0, -2)
+
+				tile.addEventListener("animationstart", () => {
+					setTimeout(() => {
+						tile.dataset.state = state
+
+						setTimeout(() => {
+							tile.classList.remove("flip")
+						}, animationTime / 2)
+					}, animationTime / 2)
+				})
+			})
+			;[...tiles].pop().addEventListener("animationend", resolve)
+		})
+
+		await gridFlip
+
+		// KEYBOARD ITEMS
+
+		colorStates.forEach(([state, letter], index) => {
+			const key = keys.find((key) => key.textContent == letter)
+			// green
+			// yellow
+			// black
+
+			if (state == "correct") key.dataset.state = state
+			else if (state == "present") key.dataset.state = state
+			else key.dataset.state = state
+		})
 	}
 
-	//enter -> get the guess ->
-	guessWord(guess){
-		if(this.validateWord(guess)){
+	displayModal(message) {
+		const queue = this.modalQueue
+		if (
+			queue.slice().pop() &&
+			queue.slice().pop().getBoundingClientRect().bottom >
+				document.body.clientHeight
+		)
+			return
 
-		} else{
+		const modal = document.createElement("div")
+		const modalContainer = document.querySelector(".modal-container")
 
+		modal.textContent = message
+		modal.className = "modal"
+
+		modalContainer.append(modal)
+		queue.push(modal)
+
+		setTimeout(() => {
+			const lastChild = queue.pop()
+			lastChild.classList.add("fadingOut")
+			lastChild.addEventListener("animationend", () => {
+				lastChild.remove()
+			})
+		}, 1500)
+	}
+
+	async guessWord(guess) {
+		const wordIsValid = await this.validateWord(guess)
+
+		if (wordIsValid) {
+			if (this.attempts == 0 && this.answer == null) {
+				this.answer = await this.generateWord()
+				console.log(this.answer)
+			}
+
+			await this.checkLetters(guess, this.answer)
+
+			this.attempts++
+			this.currentGuess = []
+
+			if (guess == this.answer) {
+				// GAME OVER
+				this.gameOver = true
+				this.displayModal(this.winMessages[this.attempts - 1])
+			}
+		} else {
+			this.currentRow.classList.add("shake")
+			setTimeout(() => {
+				this.currentRow.classList.remove("shake")
+			}, 600)
 		}
+	}
+
+	get currentRow() {
+		return [...document.querySelector(".grid").children][this.attempts]
 	}
 }
--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,52 @@
-import Game from "./Game.js";
-const Wordle = new Game()
-Wordle.createKeys()
-Wordle.createGrid()
+import Game from "./Game.js"
+const Wordle = new Game().init()
+
+// SETTING ANSWER
+
+// ADDING LETTERS
+
+function inputLetter(letter) {
+	if (Wordle.gameOver) return
+	if (/\b[a-z]\b/.test(letter)) {
+		if (Wordle.currentGuess.length == 5) return
+		const currentRow = Wordle.currentRow
+
+		const tile = currentRow.children[Wordle.currentGuess.length]
+		Wordle.currentGuess.push(letter)
+		tile.textContent = letter
+	}
+}
+
+// DELETING AND ENTERING LETTERS
+
+function deleteOrEnter(key){
+	if (Wordle.gameOver) return
+	if (key == "Enter") {
+		Wordle.guessWord(Wordle.currentGuess.join(""))
+	} else if (key == "Backspace") {
+		if (Wordle.currentGuess.length == 0) return
+
+		const currentRow = Wordle.currentRow
+		const tile = currentRow.children[Wordle.currentGuess.length - 1]
+		tile.textContent = ""
+
+		Wordle.currentGuess.pop()
+	}
+}
+
+// PRESSING ON KEYBOARD
+document.addEventListener("keypress", (e) => inputLetter(e.key))
+document.addEventListener("keydown", (e) => deleteOrEnter(e.key))
+
+// CLICKING ONSCREEN KEYBOARD
+
+document.querySelectorAll(".key").forEach(key => {
+	key.addEventListener("click", (e) => {
+		const datasetValue = e.target.dataset.value
+		if(datasetValue){
+			deleteOrEnter(e.target.dataset.value)
+		} else{
+			inputLetter(e.target.textContent)
+		}
+	})
+})
